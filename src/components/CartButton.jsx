7f0dc86fb2a73484b79a2585647d7514352ac0a2
Ewerton dos anjos ribeiro <ewertondosanjosz@gmail.com>
2022-09-21 10:36:30 -0300

    Começo da funcionalidade de Slider do Carrinho

--- src/components/CartButton.jsx
+++ src/components/CartButton.jsx
@@ -1,22 +1,20 @@
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable no-return-assign */
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
 import CartIcon from '../assets/carrinho.svg';
-
 
 import '../styles/CartButton.css';
 
-function CartButton() {
+function CartButton({ ...rest }) {
   const cart = useSelector((state) => state.cart.cart);
-  const cartTotal = cart.reduce((acc, cur) => acc += cur.quantity, 0)
+  // eslint-disable-next-line no-param-reassign
+  const cartTotal = cart.reduce((acc, cur) => (acc += cur.quantity), 0);
+
   return (
-    <div className="cart-button">
-      <Link to="/shopcart" data-testid="shopping-cart-button">
-        <img src={CartIcon} alt="Carrinho de Compras" />
-        <div className="cart-quantity">
-          {cartTotal}
-        </div>
-      </Link>
+    <div className="cart-button" {...rest}>
+      <img src={CartIcon} alt="Carrinho de Compras" />
+      <div className="cart-quantity">{cartTotal}</div>
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,21 @@
-import PropTypes from 'prop-types';
-import { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { FaSearch } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 import { setProducts } from '../redux/features/products';
 import { getProductsFromCategoryAndQuery } from '../services/api';
 import CartButton from './CartButton';
 
 import Logo from '../assets/logo.svg';
 
-import { FaSearch } from "react-icons/fa"
-
 import '../styles/Header.css';
-import { Link } from 'react-router-dom';
 
 function Header() {
   const [search, setSearch] = useState('');
+  const [slider, setSlider] = useState(true);
 
   const dispatch = useDispatch();
+  const cart = useSelector((state) => state.cart.cart);
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -26,31 +26,39 @@ function Header() {
   }
 
   return (
-    <header className="header">
-      <div className="header-container">
-        <form className="search-wrapper" onSubmit={handleSubmit}>
-          <input
-            type="text"
-            placeholder="Digite a sua Busca"
-            data-testid="query-input"
-            onChange={(e) => setSearch(e.target.value)}
-          />
-          <FaSearch />
-        </form>
-        <Link to="/">
-          <div className="header-logo">
-            <img src={Logo} alt="logo" />
-          </div>
-        </Link>
-        <CartButton />
-      </div>
-    </header>
+    <>
+      <header className="header">
+        <div className="header-container">
+          <form className="search-wrapper" onSubmit={handleSubmit}>
+            <input
+              type="text"
+              placeholder="Digite a sua Busca"
+              data-testid="query-input"
+              onChange={(e) => setSearch(e.target.value)}
+            />
+            <FaSearch />
+          </form>
+          <Link to="/">
+            <div className="header-logo">
+              <img src={Logo} alt="logo" />
+            </div>
+          </Link>
+          <CartButton onClick={() => setSlider(!slider)} />
+        </div>
+      </header>
+      {slider && (
+        <div className="slider">
+          {cart.length === 0 ? (
+            <div className="message-container">
+              <h1>Seu Carrinho Esta Vazio</h1>
+            </div>
+          ) : (
+            <h1>Carrinho </h1>
+          )}
+        </div>
+      )}
+    </>
   );
 }
-
-Header.propTypes = {
-  handleSearchChange: PropTypes.func,
-  handleClick: PropTypes.func,
-}.isRequired;
 
 export default Header;
